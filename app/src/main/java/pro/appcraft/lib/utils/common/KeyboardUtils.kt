9931d339068b412a92c847b26360de15b54563c9
TyartN <nikita.tyart@appcraft.team>
2020-12-09 12:01:34 +0300

    Some more keyboard utils

--- app/src/main/java/pro/appcraft/lib/utils/common/KeyboardUtils.kt
+++ app/src/main/java/pro/appcraft/lib/utils/common/KeyboardUtils.kt
@@ -36,3 +36,15 @@ fun Activity?.showKeyboard(forced: Boolean = false) {
     hideKeyboard()
     toggleKeyboard(forced)
 }
+
+fun Fragment?.showKeyboard(view: View, forced: Boolean = false) =
+    this?.activity.showKeyboard(view, forced)
+
+fun Activity?.showKeyboard(view: View, forced: Boolean = false) {
+    view.requestFocus()
+    val imm = this?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+    imm?.showSoftInput(
+        view,
+        if (forced) InputMethodManager.SHOW_FORCED else InputMethodManager.SHOW_IMPLICIT
+    )
+}
